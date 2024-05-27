ecd154c273c813b625654e9d1563b0d4bf1a360c
Tau_mas <112768805+Nokturnnn@users.noreply.github.com>
2024-05-27 17:28:19 +0200

    Test_Toms

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Web App with Python Flask!'
+def hello_world():
+    return 'Hello, World!'
 
-app.run(host='0.0.0.0', port=81)
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 81))  # Get the port number from the PORT environment variable, default to 81 if not set
+    app.run(host='0.0.0.0', port=port)
